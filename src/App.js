6042bd8bf67c8617589b0ee7003ec86e5ac6ce8f
mikaellaulainen <laulainenmikael@gmail.com>
2023-03-30 11:37:42 +0300

    Updated ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form, Row } from 'react-bootstrap'
 import Country from './components/Country'
 import countryService from './services/getcountries'
 
@@ -13,21 +13,22 @@ const App = () => {
     countryService.getCountries(country).then(res => {
       setCountries(res)
     })
+    setCountry('')
   }
   return (
     <div className="App">
-      <Form onSubmit={getResult} className='col-4 mx-auto border p-3 my-5'>
+      <Form onSubmit={getResult} className='col-11 col-sm-9 col-md-6 col-lg-4 mx-auto border p-3 my-5'>
         <Form.Group>
           <Form.Label>Country name</Form.Label>
           <Form.Control value={country} onChange={({ target }) => setCountry(target.value)} type="text" placeholder='Write country'/>
         </Form.Group>
         <Button className='mt-3' type='submit'>Search</Button>
       </Form>
-      <div>
+      <Row className='w-100 text-center justify-content-center mx-auto'>
         {countries.map(item =>
           <Country key={item.area} item={item}/>
         )}
-      </div>
+      </Row>
     </div>
   )
 }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import { Card } from 'react-bootstrap'
 
 const Country = ({ item }) => {
+  const [view,setView] = useState(false)
   console.log(item.area)
   return (
-    <Card>
+    <Card className='col-11 col-sm-9 col-md-5 col-lg-3 col-xl-2 p-3 m-3'>
+      <Card.Title>{item.name.common}</Card.Title>
       <Card.Img className="card-img" src={item.flags.png}/>
       <Card.Body>
-        <Card.Title>{item.name.common}</Card.Title>
-        <Card.Text>
-          {item.population}
-        </Card.Text>
+        <button onClick={() => setView(!view)}>More info</button>
+        {view &&
+        <div>
+          <Card.Text>
+            Population: {item.population}
+          </Card.Text>
+          <Card.Text>
+            Capital: {item.capital}
+          </Card.Text>
+        </div>
+
+        }
+
       </Card.Body>
     </Card>
   )
